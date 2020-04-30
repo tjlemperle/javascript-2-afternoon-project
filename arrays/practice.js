e0523598de6f7ec1a35453bf74184f3007e13fd6
tjlemperle <tjlemperle@gmail.com>
2020-04-30 15:32:12 -0600

    updated

--- arrays/practice.js
+++ arrays/practice.js
@@ -14,7 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: 'Travis', 
+  age: 29
+}
+
+// alert(me)
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +28,31 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: 'Queen', 
+  food: 'Fast',
+  person: 'Me', 
+  book: 'How to be less awesome',
+  movie: 'Count of MonteCristo',
+  holiday: 'Thanksgiving'
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'Metro'
+favoriteThings.brand = 'Geo'
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings['food'] = 'Chicken Nuggets'
+favoriteThings['book'] = 'Harry Potter'
+console.log(favoriteThings)
 
 ////////// PROBLEM 3 //////////
 
@@ -49,13 +64,21 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
+let backPack = {
+
+}
+
+let item = 'firstPocket'
+
+backPack[item] = 'chapstick'
+
+backPack.color = 'black'
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+// alert(backPack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +86,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -84,7 +107,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -95,14 +119,15 @@ var user2 = {
   Create an empty object called methodCollection.
 */
 
-//Code Here
+let methodCollection = {}
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
 
-//Code Here
+methodCollection.alertHello = () => alert('hello')
+methodCollection.logHello = () => console.log('hello')
 
 /*
   Now call your alertHello and logHello methods.
@@ -117,7 +142,13 @@ var user2 = {
   Return a new object with all of the information that you passed in.
 */
 
-//Code Here
+let makePerson = (name, birthday, ssn) => {
+  return personInfo = {
+    name,
+    birthday,
+    ssn
+  }
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -126,4 +157,10 @@ var user2 = {
   Return that object so that whenever you invoke makeCard, you get a brand new credit card.
 */
 
-//Code Here
+let makeCard = (cardNumber, expirationDate, securityCode) => {
+  return newCard = {
+    cardNumber,
+    expirationDate,
+    securityCode
+  }
+}
--- objects/practice.js
+++ objects/practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+let first = (arr) => {
+  return arr.shift()
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+let last = (arr) => {
+  return arr.pop()
+}
 
 
 
@@ -47,7 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+let looper = (family) => {
+  for(let i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 
 
@@ -62,7 +70,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+let reversedLooper = (letters) => {
+  for(let i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 
@@ -77,7 +89,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+let evenFinder = (nums) => {
+  
+  let evens = []
+
+  for(let i = 0; i < nums.length; i++){
+    nums[i] % 2 === 0 ? evens.push(nums[i]): null;
+}
+
+return evens
+}
 
 
 
